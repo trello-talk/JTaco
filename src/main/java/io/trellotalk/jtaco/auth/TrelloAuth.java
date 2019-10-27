60369df75fc105b3212482678f75ad2e2afecbda
Matthew Cain <yooksi@zoho.com>
2019-10-27 09:39:40 +0100

    Add Trello authentication class

---
+++ src/main/java/io/trellotalk/jtaco/auth/TrelloAuth.java
@@ -0,0 +1,16 @@
+package io.trellotalk.jtaco.auth;
+
+import io.trellotalk.jtaco.Config;
+
+public class TrelloAuth {
+
+    private final String API_KEY = Config.getValue(Config.Key.TRELLO_API_KEY);
+    private final String TOKEN = Config.getValue(Config.Key.TRELLO_TOKEN);
+
+    public String getApiKey() {
+        return API_KEY;
+    }
+    public String getToken() {
+        return TOKEN;
+    }
+}
